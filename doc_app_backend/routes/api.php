631d93312033694be69ee6ee7136ffda87418ecd
Pnoccio <robinshango@gmail.com>
2024-01-26 14:51:37 +0300

    ..

--- doc_app_backend/routes/api.php
+++ doc_app_backend/routes/api.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\appointmentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::get('/user', [UserController::class, 'index']);
+    Route::get('/book', [appointmentController::class], 'store');
 });
 
 
--- lib/screens/booking_page.dart
+++ lib/screens/booking_page.dart
@@ -1,12 +1,15 @@
 // ignore_for_file: unused_import
 
+import 'package:doc_app/provider/dio_provider.dart';
 import 'package:doc_app/utils/config.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 import '../components/button.dart';
 import '../components/custom_app_bar.dart';
+import '../models/booking_datetime_converted.dart';
 
 class BookingPage extends StatefulWidget {
   const BookingPage({super.key});
@@ -23,9 +26,23 @@ class _BookingPageState extends State<BookingPage> {
   bool _isWeekend = false;
   bool _dateSelected = false;
   bool _timeSelected = false;
+  String? token;
+
+  Future<void> getToken() async {
+    final SharedPreferences preferences = await SharedPreferences.getInstance();
+    token = preferences.getString('token') ?? '';
+  }
+
+  @override
+  void initState() {
+    getToken();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     Config().init(context);
+    final doctor = ModalRoute.of(context)!.settings.arguments as Map;
     return Scaffold(
       appBar: const CustomAppBar(
         appTitle: 'Appointment',
@@ -116,7 +133,14 @@ class _BookingPageState extends State<BookingPage> {
             child: Button(
               width: double.infinity,
               title: 'Make Appointment',
-              onPressed: () {
+              onPressed: () async {
+                // there is need to convert date/day/time into string first
+                final getDate = DateConverted.getDate(_currentDay);
+                final getDay = DateConverted.getDay(_currentDay.weekday);
+                final getTime = DateConverted.getTime(_currentIndex!);
+
+                final booking = await DioProvider().bookAppointment(
+                    getDate, getDay, getTime, doctor['doctor_id'], token!);
                 Navigator.of(context).pushNamed('success_booking');
               },
               disable: _timeSelected && _dateSelected ? false : true,
